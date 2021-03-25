4371286e35bbb22494716e35af65173ea8a65053
Isac Byeonghoon Yoo <isac.yoo@buzzvil.com>
2021-03-25 14:40:21 +0900

    Bump up version to 0.2.11

--- src/buzzvil_python_styleguide/__init__.py
+++ src/buzzvil_python_styleguide/__init__.py
@@ -1,6 +1,6 @@
 # coding: UTF-8
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-__version__ = '0.2.10'
+__version__ = '0.2.11'
 
 from buzzvil_python_styleguide.plugin import RequestsTimeoutPlugin  # noqa: F401
