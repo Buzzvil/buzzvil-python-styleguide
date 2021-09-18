68ffdad683bde6da655733d7d37d2799a3df51e0
Isac Byeonghoon Yoo <isac.yoo@buzzvil.com>
2021-09-18 15:48:37 +0900

    🔥 - Remove python 2 constraints

--- src/buzzvil_python_styleguide/__init__.py
+++ src/buzzvil_python_styleguide/__init__.py
@@ -1,6 +1,3 @@
-# coding: UTF-8
-from __future__ import absolute_import, division, print_function, unicode_literals
-
 __version__ = '0.3.1'
 
 from buzzvil_python_styleguide.plugin import RequestsTimeoutPlugin  # noqa: F401
--- tests/test_flake8_buzzvil.py
+++ tests/test_flake8_buzzvil.py
@@ -1,6 +1,3 @@
-# coding: UTF-8
-from __future__ import absolute_import, division, print_function, unicode_literals
-
 from buzzvil_python_styleguide import __version__
 
 
