28e622f076fbe81f6313a2d0eb0eb11ecad24294
Isac Byeonghoon Yoo <isac.yoo@buzzvil.com>
2021-10-11 22:17:13 +0900

    ✅ - Add unit test for RequestsTimeoutPlugin

--- tests/test_func_call_visitor.py
+++ tests/test_func_call_visitor.py
@@ -2,7 +2,7 @@ import ast
 
 import pytest
 
-from buzzvil_python_styleguide.plugin import FuncCallVisitor
+from buzzvil_python_styleguide.plugin import FuncCallVisitor, RequestsTimeoutPlugin
 
 
 class TestFucCallVisitor:
@@ -25,3 +25,53 @@ class TestFucCallVisitor:
         tree = ast.parse(source)
         v.visit(tree)
         assert expected == v.name
+
+
+class TestRequestsTimeoutPlugin:
+    _ERR_MSG = 'BZ1: timeout keyword is required for requests.get and requests.post'
+
+    @pytest.mark.parametrize(
+        ('invocation_line', 'is_lint_target'),
+        (
+            ('requests.post', True),
+            ('requests.get', True),
+            ('session.post', True),
+            ('session.get', True),
+            ('requests\\\n\t.get', True),
+            ('requests.\\\n\tget', True),
+            ('request.post', False),
+            ('request.get', False),
+        ),
+    )
+    @pytest.mark.parametrize(
+        ('source', 'expected'),
+        (
+            (
+                '{invocation_line}(\'some_url\', timeout=test())',
+                (),
+            ),
+            (
+                '{invocation_line}(\'some_url\')',
+                ((1, 0, _ERR_MSG, RequestsTimeoutPlugin),),
+            ),
+            (
+                'x = {invocation_line}(\'some_url\')',
+                ((1, 4, _ERR_MSG, RequestsTimeoutPlugin),),
+            ),
+            (
+                'with {invocation_line}(\'some_url\') as res:\n  pass',
+                ((1, 5, _ERR_MSG, RequestsTimeoutPlugin),),
+            ),
+            (
+                'with {invocation_line}(\'some_url\', timeout=test()) as res:\n  pass',
+                (),
+            ),
+        ),
+    )
+    def test_run(self, invocation_line, is_lint_target, source, expected):
+        tree = ast.parse(source.format(invocation_line=invocation_line))
+        p = RequestsTimeoutPlugin(tree)
+        if is_lint_target:
+            assert expected == tuple(p.run())
+        else:
+            assert tuple() == tuple(p.run())
